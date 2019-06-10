075e4e07fae643b5e2593e4da24b260aaf8855d8
Tim Huijser <timhuijser@gmail.com>
2019-06-11 00:52:16 +0200

    Initial Commit

---
+++ Dice Roll/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dice Roll
+//
+//  Created by Tim on 11/06/2019.
+//  Copyright © 2019 Tim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
